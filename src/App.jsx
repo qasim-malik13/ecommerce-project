ce1d58f83fcc27d8744b2122da4fb7be9bbc0a25
qasim-malik13 <qasim.malik@proseonpixels.com>
2026-04-16 00:40:14 +0530

    Created Orders and Tracking Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import HomePage from "./Pages/HomePage"
 import { Routes, Route } from 'react-router'
 import CheckoutPage from "./Pages/CheckoutPage.Jsx"
+import OrdersPage from "./Pages/OrdersPage.jsx"
+import TrackingPage from "./Pages/TrackingPage.jsx"
 
 function App() {
 
@@ -9,6 +11,8 @@ function App() {
     <Routes>
       <Route index element={<HomePage/>}/>
       <Route path='/checkout' element={<CheckoutPage/>}/>
+      <Route path='/orders' element={<OrdersPage/>}/>
+      <Route path='/tracking' element={<TrackingPage/>}/>
     </Routes>
   )
 }
