fcd0c86d3d18ff9c7d4a66f0a0bf92eea4fdda40
qasim-malik13 <qasim.malik@proseonpixels.com>
2026-04-16 00:15:45 +0530

    Created Multiple Pages using React Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import HomePage from "./Pages/HomePage"
+import { Routes, Route } from 'react-router'
 
 function App() {
 
 
   return (
-    <>
-    <HomePage/>
-    </>
+    <Routes>
+      <Route index element={<HomePage/>}/>
+      <Route path='/checkout' element={<div>Check-out</div>}/>
+    </Routes>
   )
 }
 
