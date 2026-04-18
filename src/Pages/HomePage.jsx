0d0acd2e5220514feb0f4a3c2843039b1f029faa
qasim-malik13 <qasim.malik@proseonpixels.com>
2026-04-18 14:11:35 +0530

    Use Data from backend to generate HTML

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,11 @@
 import "./HomePage.css";
 import Header from "../components/Header";
-import { products } from "../../starting-files/data/products";
 import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function HomePage() {
-
   // Using Fetch API to get products data from the backend server
- /*
+  /*
   fetch("http://localhost:3000/api/products").then((response) => {
     return response.json()
   }).then((data) => {
@@ -14,11 +13,14 @@ export default function HomePage() {
     });
     */
 
-  // Using Axios to get products data from the backend server
-  axios.get("http://localhost:3000/api/products")
-  .then((response) => {
-    console.log(response.data);
-  });
+  const [products, setProducts] = useState([]);
+
+  // using useEffect to call the API when the component first mounts
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
 
   return (
     <>
