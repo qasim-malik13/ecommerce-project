6f2eda708f7fc872aca6663614265391a66e79aa
qasim-malik13 <qasim.malik@proseonpixels.com>
2026-04-18 14:26:35 +0530

    Lift cart up bw Pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,22 @@ import { Routes, Route } from 'react-router'
 import CheckoutPage from "./Pages/CheckoutPage.Jsx"
 import OrdersPage from "./Pages/OrdersPage.jsx"
 import TrackingPage from "./Pages/TrackingPage.jsx"
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
+    const [cart, setCart] = useState([]);
+
+    useEffect(()=> {
+       axios.get("/api/cart-items").then((response) => {
+        setCart(response.data);
+    })
+    }, [])
 
   return (
     <Routes>
-      <Route index element={<HomePage/>}/>
-      <Route path='/checkout' element={<CheckoutPage/>}/>
+      <Route index element={<HomePage cart = {cart}/>}/>
+      <Route path='/checkout' element={<CheckoutPage cart = {cart}/>}/>
       <Route path='/orders' element={<OrdersPage/>}/>
       <Route path='/tracking' element={<TrackingPage/>}/>
     </Routes>
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-export default function HomePage() {
+export default function HomePage({cart}) {
   // Using Fetch API to get products data from the backend server
   /*
   fetch("http://localhost:3000/api/products").then((response) => {
@@ -14,17 +14,12 @@ export default function HomePage() {
     */
 
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
 
   // using useEffect to call the API when the component first mounts
   useEffect(() => {
     axios.get("/api/products").then((response) => {
       setProducts(response.data);
     });
-
-    axios.get("/api/cart-items").then((response) => {
-        setCart(response.data);
-    })
   }, []);
 
   return (
