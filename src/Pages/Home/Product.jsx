2191d8ffc89170ee89082cbdc4d891ba22cd2721
qasim-malik13 <qasim.malik@proseonpixels.com>
2026-04-19 16:51:48 +0530

    Create integration test for product component

--- src/Pages/Home/Product.jsx
+++ src/Pages/Home/Product.jsx
@@ -20,7 +20,9 @@ const Product = ({ product, loadCart }) => {
   return (
     <div className="product-container">
       <div className="product-image-container">
-        <img className="product-image" src={product.image} />
+        <img className="product-image" 
+        data-testid="product-image"
+        src={product.image} />
       </div>
 
       <div className="product-name limit-text-to-2-lines">{product.name}</div>
@@ -28,6 +30,7 @@ const Product = ({ product, loadCart }) => {
       <div className="product-rating-container">
         <img
           className="product-rating-stars"
+          data-testid="product-rating-stars"
           src={`images/ratings/rating-${product.rating.stars * 10}.png`}
         />
         <div className="product-rating-count link-primary">
@@ -63,7 +66,7 @@ const Product = ({ product, loadCart }) => {
         Added
       </div>
 
-      <button className="add-to-cart-button button-primary" onClick={addToCart}>
+      <button className="add-to-cart-button button-primary" onClick={addToCart} data-testid="addToCartButton">
         Add to Cart
       </button>
     </div>
