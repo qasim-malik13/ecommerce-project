daf9a497fded02ca7745e1d94d345215ae1c0c4e
qasim-malik13 <qasim.malik@proseonpixels.com>
2026-04-19 16:18:25 +0530

    Make create order work

--- src/Pages/Checkout/PaymentSummary.jsx
+++ src/Pages/Checkout/PaymentSummary.jsx
@@ -1,7 +1,17 @@
-import React from "react";
-import { formatMoney } from "../../utils/money";
 
-const PaymentSummary = ({ paymentSummary }) => {
+import { formatMoney } from "../../utils/money";
+import { useNavigate } from "react-router";
+import axios from "axios";
+
+const PaymentSummary = ({ paymentSummary, loadCart }) => {
+  
+  const navigate = useNavigate();
+
+  const createOrder = async () => {
+    await axios.post("/api/orders")
+    await loadCart();
+    navigate("/orders");
+  }
   return (
     <div className="payment-summary">
       <div className="payment-summary-title">Payment Summary</div>
@@ -43,7 +53,8 @@ const PaymentSummary = ({ paymentSummary }) => {
             </div>
           </div>
 
-          <button className="place-order-button button-primary">
+          <button className="place-order-button button-primary"
+          onClick={createOrder}>
             Place your order
           </button>
         </>
